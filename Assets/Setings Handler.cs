a136a9ae10615b76743207cb43699fea983504cd
MAtv3y15 <matvejsorohov05@gmail.com>
2025-01-15 18:32:32 +0500

    Music Volume Change

---
+++ Assets/Setings Handler.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetingsHandler : MonoBehaviour
+{
+    public void ChangeMusicVolume()
+    {
+
+    }
+}
