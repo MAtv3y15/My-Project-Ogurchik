1f71540ea19ccf4b03f3d0a2b9c15883185902b1
MAtv3y15 <matvejsorohov05@gmail.com>
2025-02-28 16:53:56 +0500

    player prefs
    csf

--- Assets/Setings Handler.cs
+++ Assets/Setings Handler.cs
@@ -1,11 +1,45 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 using UnityEngine;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;  
 
-public class SetingsHandler : MonoBehaviour
+public class SettingsHandler : MonoBehaviour
 {
-    public void ChangeMusicVolume()
+    public Slider musicVolumeSider;
+    public AudioMixer musicMixer;
+    public Slider SFXSlider;
+    private void Start()
     {
+        musicVolumeSider.value = PlayerPrefs.GetFloat("MusicVolume");
+
+        SFXSlider.value = PlayerPrefs.GetFloat("SFXVolume");
+
 
     }
+
+    public void ChangeMusicVolume()
+    {
+        musicMixer.SetFloat("MusicVolume", musicVolumeSider.value);
+        PlayerPrefs.SetFloat("MusicVolume", musicVolumeSider.value);
+    }
+   
+    public void ChangeSFXVolume()
+    {
+        musicMixer.SetFloat("SFXVolume", SFXSlider.value);
+        PlayerPrefs.SetFloat("SFXVolume", SFXSlider.value);
+    }
+    
+    
+
+
+
+
+
+
+
+
 }
+
